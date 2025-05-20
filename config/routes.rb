2d172bfef1f573638b850f6f832f977ea8b1de67
Kaho Maeda <kaho@tutamail.com>
2025-05-20 16:43:43 +0900

    some updates on bookings/index and show, added show in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :show]
 end
