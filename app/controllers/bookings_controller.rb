5a7722ed4d5e5e948cb4bcdc4fbb05862526f126
Naw San <nawsanbmo@gmail.com>
2025-05-21 16:36:04 +0900

    deleted raise

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,7 +17,6 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to bookings_path
     else
-      raise
       render "spaceships/show", status: :unprocessable_entity
     end
   end
