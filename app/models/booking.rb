33a952f55b5e98cbc12cad90fa366461012466f5
Nathaniel Smith <naffsmith@hotmail.co.uk>
2025-05-22 17:31:22 +0900

    All the photos are there now.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -15,8 +15,6 @@ class Booking < ApplicationRecord
   scope :completed, -> { accepted.past }
   scope :not_rejected, -> { where.not(status: :rejected) }
 
-  @booking.status = "pending"
-
   def number_of_days
     ( end_date - start_date ).to_i
   end
