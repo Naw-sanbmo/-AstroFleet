46e7ced11660c6ed4551b113197147cfdaf74de8
Nathaniel Smith <naffsmith@hotmail.co.uk>
2025-05-23 13:15:36 +0900

    Added a new ship function to owner page.

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -3,4 +3,10 @@ class Spaceship < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
   has_many :bookings
+
+  private
+
+  def photo_presence
+    errors.add(:photo, "must be attached") unless photo.attached?
+  end
 end
